cffbde20b44b55bcbf427ba2c31f6c88da2f46f6
Kaustubh <43691873+kaustubhgupta@users.noreply.github.com>
2020-10-04 21:39:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ exceptions : github.GithubException.UnknownObjectException
 g = Github(sys.argv[1])
 repo_list = [repo.name for repo in g.get_user().get_repos()]
 print(g.get_repo('kaustubhgupta/' + str(repo_list[-3])).get_topics())
-with open('test.txt', 'w') as f:
+with open('test.txt', 'a') as f:
     f.write(str(g.get_repo('kaustubhgupta/' + str(repo_list[-1])).get_topics()))
 
 
