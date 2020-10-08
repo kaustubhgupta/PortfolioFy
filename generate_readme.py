a76467aad9e5e6c06d3faf73350c39664b217ea1
Kaustubh <43691873+kaustubhgupta@users.noreply.github.com>
2020-10-08 21:38:44 +0530

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -11,20 +11,73 @@ with open('data_hackathon.json') as json_file:
 
 
 css = '''
-.button {
-  background-color: #f1f8ff;
-  border: none;
-  color: #0366d6;
-  padding: 10px 20px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  margin: 4px 2px;
-  cursor: pointer;
-  border-radius: 16px;
+.footer{
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color:white;
+   color: black;
+   text-align: center;
 }
-.img-rounded{
-    border-radius: 50%;
+
+a {
+        background-color: transparent;
+        text-decoration: none;
+}
+
+.a-blue {
+        color: #0366d6;
+        background-color: transparent;
+        text-decoration: none;
+}
+
+.button {
+        background-color: #f1f8ff;
+        border: none;
+        color: #0366d6;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        margin: 4px 2px;
+        cursor: pointer;
+        border-radius: 16px;
+}
+
+.img-rounded {
+        border-radius: 50%;
+}
+
+h1 {
+        font-size: 250%
+}
+
+h2 {
+        font-size: 150%
+}
+
+.h2-repo {
+        font-size: 180%
+}
+
+h3 {
+        font-size: 140%;
+}
+
+.div-repos {
+        width: 60%;
+}
+
+.div-main {
+        padding-top: 50px;
+        padding-right: 30px;
+        padding-bottom: 50px;
+        padding-left: 80px;
+
+}
+.text-repo{
+        font-size: 120%;
+        color:#586069;
 }
 '''
 
@@ -36,16 +89,14 @@ if len(project_data)!=0:
         topics = ''
         for topic in project_data[project]['repo_topics']:
             topics+= f'''<button class="button">{topic}</button>'''
-        project_repos += f'''<div class="container">
-        <div>
+        project_repos += f'''<div class="div-repos" align="left" id={count_project}>
         <br>
-        <h4 style='color:#0366d6'><a href="https://github.com/kaustubhgupta/{project}">{project}</a></h4>
+        <h2 class="h2-repo"><a class="a-blue" href="https://github.com/kaustubhgupta/{project}">{project}</a></h2>
         {topics}
         <br>
-        <text style='color:#586069'>{project_data[project]['repo_description']}<text><br>
-        </div>
-        <img src="https://image.flaticon.com/icons/png/512/126/126482.png" width="16" height="16">{project_data[project]['repo_stars']}
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Octicons-repo-forked.svg/675px-Octicons-repo-forked.svg.png" width="16" height="16">{project_data[project]['repo_forks']}
+        <text class="text-repo">{project_data[project]['repo_description']}<text><br>
+        <img src="https://image.flaticon.com/icons/png/512/126/126482.png" width="14" height="14">{project_data[project]['repo_stars']}
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Octicons-repo-forked.svg/675px-Octicons-repo-forked.svg.png" width="14" height="14">{project_data[project]['repo_forks']}
         <br>
         </div>  
         '''
@@ -60,16 +111,14 @@ if len(hackathon_data)!=0:
         topics = ''
         for topic in hackathon_data[hackathon]['repo_topics']:
             topics+= f'''<button class="button">{topic}</button>'''
-        hackathon_repos += f'''<div class="container">
-        <div>
+        hackathon_repos += f'''<div class="div-repos" align="left" id={count_hackathon}>
         <br>
-        <h4 style='color:#0366d6'><a href="https://github.com/kaustubhgupta/{hackathon}">{hackathon}</a></h4>
+        <h2 class="h2-repo"><a class="a-blue" href="https://github.com/kaustubhgupta/{hackathon}">{hackathon}</a></h2>
         {topics}
         <br>
-        <text style='color:#586069'>{hackathon_data[hackathon]['repo_description']}<text><br>
-        </div>
-        <img src="https://image.flaticon.com/icons/png/512/126/126482.png" width="16" height="16">{hackathon_data[hackathon]['repo_stars']}
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Octicons-repo-forked.svg/675px-Octicons-repo-forked.svg.png" width="16" height="16">{project_data[project]['repo_forks']}
+        <text class="text-repo">{hackathon_data[hackathon]['repo_description']}<text><br>
+        <img src="https://image.flaticon.com/icons/png/512/126/126482.png" width="14" height="14">{hackathon_data[hackathon]['repo_stars']}
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Octicons-repo-forked.svg/675px-Octicons-repo-forked.svg.png" width="14" height="14">{hackathon_data[hackathon]['repo_forks']}
         <br>
         </div>  
         '''
@@ -77,35 +126,41 @@ if len(hackathon_data)!=0:
 
 
 template = f'''
+<html>
+<title>{user_data['name']} Portfolio</title>
+<head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <style>{css}</style>
+</head>
 <body>
-<div align="center">
-<img class="img-rounded" align="center" alt="Image" height="250px" src="{user_data['git_photo_url']}" />
+<div class="div-main" align="center">
+<img class="img-rounded" alt="Image" height="350px" src="{user_data['git_photo_url']}" />
 <h1 style='color:#f24b4b'> Hi, I am <a href="https://github.com/{user_data['username']}">{user_data['name']}</a>👨‍🎓 </h1>
-<h6 style='color:#4a90ed'>{user_data['git_bio']}</h6>
-<img  src="https://github-readme-stats.vercel.app/api?username={user_data['username']}&show_icons=true&theme=radical&count_private=true">
-<h5 style='color:#697d3b'>Followers: <b>{user_data['git_followers']}</b>, Following: <b>{user_data['git_following']}</b></h5>
+<h2 style='color:#21439e'>{user_data['git_bio']}</h2>
+<img height="300px" src="https://github-readme-stats.vercel.app/api?username={user_data['username']}&show_icons=true&theme=radical&count_private=true">
+<h3 style='color:#751878'>Followers: <b>{user_data['git_followers']}</b>, Following: <b>{user_data['git_following']}</b></h3>
 <br>
-<h2 style="color:#d45131"> My Projects👇</h2>
-<text style='font-size:16px'>(These repositories are tagged as project. *Updated: {user_data['latest_updated']})</text>
-<div class="mx-auto my-auto" align="left">{project_repos}</div>
+<h1 style="color:#d45131"> My Projects👇</h1>
+<text style='font-size:100%; color:#586069;'>(These repositories are tagged as project. *Updated: {user_data['latest_updated']})</text>
 <br>
-<h2 style="color:#d45131"> Hackathons I participated👇</h2>
-<text style='font-size:16px'>(These repositories are tagged as Hackathon. *Updated: {user_data['latest_updated']})</text>
-<div class="mx-auto my-auto" align="left">{hackathon_repos}</div>
+{project_repos}
 <br>
-<h2 style="color:#d45131"> My Latest Blogs✍️</h2>
-<div class="mx-auto my-auto" align="left">
+<h1 style="color:#d45131"> Hackathons I participated👇</h1>
+<text style='font-size:100%; color:#586069;'>(These repositories are tagged as project. *Updated: {user_data['latest_updated']})</text>
+<br>
+{hackathon_repos}
+<br>
+<h1 style="color:#d45131"> My Latest Blogs✍️</h1>
 <!-- BLOG-POST-LIST:START -->
 <!-- BLOG-POST-LIST:END -->
+<h3 style="color:#9a08c7">Contact on Email: {user_data['git_email']}</h3>
+</div>
+<div class="footer">
+<p style="font-size: 100%;">Generated by <a href="https://github.com/kaustubhgupta">Kaustubh Gupta</p>
 </div>
 </body>
-<footer>
-<h6>Contact on Email: {user_data['git_email']}</h6>
-</footer>
-</div>
+</html>
 '''
 
-with open('README.md', 'w', encoding='utf-8') as f:
+with open('index.html', 'w', encoding='utf-8') as f:
     f.write(template)
