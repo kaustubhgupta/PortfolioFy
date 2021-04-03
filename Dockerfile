04ec52650b3a9e6706e0b129b82c616e3ae8f99c
Kaustubh Gupta <kaustubhgupta1828@gmail.com>
2021-04-03 17:47:39 +0530

    Auto Committer (#29)
    * Fixing issue #28
    
    * Update action.yml
    
    * Update generate_index.py

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3
-
-RUN pip install -Iv PyGithub==1.53
-COPY ./ /PortfolioFy
-
-ENTRYPOINT ["python", "/PortfolioFy/generate_index.py"]
--- generate_index.py
+++ generate_index.py
@@ -92,12 +92,33 @@ else:
 social_data = strtoLinksHTML(social_links, user_data['username'])
 
 if theme_selected == '1':
-    indexFile = genHTMLLevel1(
+    newIndex = genHTMLLevel1(
         user_data, project_repos, hackathon_repos, blogs, stats_choice, social_data, resume_link, allow_footer)
 
 elif theme_selected == '2':
-    indexFile = genHTMLLevel2(user_data, project_repos,
+    newIndex = genHTMLLevel2(user_data, project_repos,
                               hackathon_repos, blogs, stats_choice, social_data, resume_link, allow_footer)
 
-with open('index.html', 'w', encoding='utf-8') as f:
-    f.write(indexFile)
+
+currentRepoName = sys.argv[9].split('/')[-1]
+
+if 'index.html' in os.listdir(sys.argv[9]):
+
+    index_path = sys.argv[9] + '/index.html'
+    with open(index_path, 'r', encoding='utf-8') as f:
+        oldIndex = f.read()
+
+    indexRepo = git.get_repo(f"{git_username}/{currentRepoName}")
+    oldContents = indexRepo.get_contents('index.html')
+
+    if oldIndex != newIndex:
+        print("Index Contents Updated")
+        indexRepo.update_file(oldContents.path, "Updating Index file",
+                        newIndex, oldContents.sha)
+    else:
+        print("No changes Detected")
+
+else:    
+    print("Writing index.html for first time")
+    indexRepo = git.get_repo(f"{git_username}/{currentRepoName}")
+    indexRepo.create_file('index.html', "Adding index file", newIndex, branch="master")
