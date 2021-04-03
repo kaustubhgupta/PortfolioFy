f68f83775829aab9a8a37ba633ab7965d8da53e2
Kaustubh Gupta <kaustubhgupta1828@gmail.com>
2021-04-03 17:59:03 +0530

    Update generate_index.py

--- generate_index.py
+++ generate_index.py
@@ -102,7 +102,7 @@ elif theme_selected == '2':
 
 currentRepoName = sys.argv[9].split('/')[-1]
 
-print(os.listdir(sys.argv[9])
+print(os.listdir(sys.argv[9]))
 if 'index.html' in os.listdir(sys.argv[9]):
 
     index_path = sys.argv[9] + '/index.html'
