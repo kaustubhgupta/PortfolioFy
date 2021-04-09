15c4a745431da5a655869aaaf0f95049ff4f280a
Kaustubh Gupta <kaustubhgupta1828@gmail.com>
2021-04-09 13:39:16 +0530

    Update addBlogs.py

--- functions/addBlogs.py
+++ functions/addBlogs.py
@@ -2,8 +2,7 @@ def addBlogsEntryL1(index_file):
     blogTemplate = '''
     <h1 style="color:#d45131"> My Latest Blogs✍️</h1>
     <div align='left' class="div-blog">
-    <!-- BLOG-POST-LIST:START -->
-    <!-- BLOG-POST-LIST:END -->
+    <!-- BLOG-POST-LIST:START --><!-- BLOG-POST-LIST:END -->
     </div>
     '''
     temp_file = index_file.replace('<!-- BLOG-ENTRY -->', blogTemplate)
@@ -18,12 +17,10 @@ def addBlogsEntryL2(index_file):
         <section class="resume-section" id="blogs">
             <div class="resume-section-content">
                 <h2 class="mb-5">Blogs</h2>
-                <!-- BLOG-POST-LIST:START -->
-                <!-- BLOG-POST-LIST:END -->
+                <!-- BLOG-POST-LIST:START --><!-- BLOG-POST-LIST:END -->
             </div>
         </section>
         <hr class="m-0" />
     '''
     final_file = temp_file.replace('<!-- BLOGS-ENTRY -->', blog_template)
     return final_file
-    
