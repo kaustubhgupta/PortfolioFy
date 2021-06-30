a68299798a139e360be03508e671f9ada2f59d2d
Kaustubh Gupta <43691873+kaustubhgupta@users.noreply.github.com>
2021-06-30 12:13:47 +0530

    Update level1.py

--- themes/level_1/level1.py
+++ themes/level_1/level1.py
@@ -12,7 +12,7 @@ def genHTMLLevel1(user_data, project_repos, hackathon_repos, blogs, stats_choice
     <title>{user_data['name']} Portfolio</title>
     <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link href="https://kaustubhgupta.github.io/themescss/level1.css" rel="stylesheet" />
+    <link href="https://www.kaustubhgupta.me/themescss/level1.css" rel="stylesheet" />
     </head>
     <body>
     <div class="div-main" align="center">
